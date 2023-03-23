cf0ceb8653667432874c1c179edaacdb63af7b93
stinglmaier1 <hammerst@hm.edu>
2023-03-23 20:31:21 +0100

    asdf

--- src/main.py
+++ src/main.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     start_point = Point(0, 0)
     end_point = Point(10, 0)
-    line = Line(start_point, end_point)
+    big_line = Line(start_point, end_point)
 
-    x_values = line.get_x_values()
-    h_values = nozzle.get_profile_for_line(line)
+    x_values = big_line.get_x_values()
+    h_values = nozzle.get_profile_for_line(big_line)
 
-    left_outer_line = nozzle.get_left_outer_line(line)
-    right_outer_line = nozzle.get_right_outer_line()
+    left_outer_line = nozzle.get_left_outer_line(big_line)
+    right_outer_line = nozzle.get_right_outer_line(big_line)
 
     plt.plot(left_outer_line.get_x_values(), left_outer_line.get_y_values(), "--k")
     plt.plot(right_outer_line.get_x_values(), right_outer_line.get_y_values(), "--k")
